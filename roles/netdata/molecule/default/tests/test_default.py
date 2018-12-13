3868d7f01bd7324f364dfa049e6976b554da4e5d
Wikitops <wikitops5692@gmail.com>
2018-12-13 15:34:56 -0500

    refact(netdata): refact playbook, update netdata version, add molecule tests, update documentation

---
+++ roles/netdata/molecule/default/tests/test_default.py
@@ -0,0 +1,36 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_sources_directories(host):
+    d = host.file("/opt/netdata")
+    assert d.exists
+    assert d.is_directory
+    assert d.user == 'root'
+    assert d.group == 'root'
+
+
+def test_conf_directories(host):
+    d = host.file("/etc/netdata")
+    assert d.exists
+    assert d.is_directory
+    assert d.user == 'root'
+    assert d.group == 'netdata'
+
+
+def test_config_file(host):
+    f = host.file("/etc/netdata/netdata.conf")
+    assert f.exists
+    assert f.is_file
+    assert f.user == 'root'
+    assert f.group == 'netdata'
+
+
+def test_service(host):
+    s = host.service('netdata')
+    assert s.is_enabled
+    assert s.is_running
